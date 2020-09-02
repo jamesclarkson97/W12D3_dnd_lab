407e7263cfd3cfb7607def8ac82d334b8fac715b
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-09-02 12:01:25 +0100

    cleric tests added

---
+++ src/test/java/ClericTest.java
@@ -0,0 +1,59 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+
+public class ClericTest {
+
+    Cleric cleric;
+
+    @Before
+    public void before() {
+        cleric = new Cleric(20);
+    }
+
+    @Test
+    public void hasHealthPoints() {
+        assertEquals(20, cleric.getHealthPoints());
+    }
+
+    @Test
+    public void hasHealingItem() {
+        HashMap<String, Integer> expected = new HashMap<String, Integer>() {{
+            put("Potion", 5);
+        }};
+        assertEquals(expected, cleric.getHealingItems());
+    }
+
+    @Test
+    public void canAddHealingItem() {
+        cleric.addHealingItems("Bandage", 1);
+        HashMap<String, Integer> expected = new HashMap<String, Integer>() {{
+            put("Potion", 5);
+            put("Bandage", 1);
+        }};
+        assertEquals(expected, cleric.getHealingItems());
+    }
+
+    @Test
+    public void canRemoveHealingItem() {
+        cleric.removeHealingItems("Potion", 3);
+        HashMap<String, Integer> expected = new HashMap<String, Integer>() {{
+            put("Potion", 2);
+        }};
+        assertEquals(expected, cleric.getHealingItems());
+    }
+
+    @Test
+    public void canUseHealingItem() {
+        assertEquals("Cleric uses a Potion", cleric.useHealingItem("Potion"));
+
+        HashMap<String, Integer> expected = new HashMap<String, Integer>() {{
+            put("Potion", 4);
+        }};
+        assertEquals(expected, cleric.getHealingItems());
+    }
+
+}
